fb4cc0f5f3d40bd92fbed576f5972395c1e65d93
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2024-07-16 15:29:01 -0500

    Merge branch 'dev-imx8'

--- apparmor_monkeys/platform.py
+++ apparmor_monkeys/platform.py
@@ -16,14 +16,24 @@ def uname():
     # run a full sh!!!
     # processor = "x86_64" if sys.maxsize > 2 ** 32 else 'i686'
     # return os.uname() + (processor, )
+    #
+    # Note: Python 3.9 changed the platform API to return the first 5 items (not processor).
+    #       So, removing it from this interface for use on systems with python >= 3.9
+    # Previous return:
+    #    processor = os_uname.machine
+    #    return platform.uname_result(system=os_uname.sysname,
+    #                             node=os_uname.nodename,
+    #                             release=os_uname.release,
+    #                             version=os_uname.version,
+    #                             machine=os_uname.machine,
+    #                             processor=processor)
+    
     os_uname = os.uname()
-    processor = os_uname.machine
     return platform.uname_result(system=os_uname.sysname,
                                  node=os_uname.nodename,
                                  release=os_uname.release,
                                  version=os_uname.version,
-                                 machine=os_uname.machine,
-                                 processor=processor)
+                                 machine=os_uname.machine)
 uname.__doc__ = _uname.__doc__
 
 
